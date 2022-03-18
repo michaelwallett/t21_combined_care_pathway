633a91e842854f036e38edd281a6cf7b79117b33
Michael Wallett <michael.x.wallett@gsk.com>
2022-03-18 14:54:37 +0000

    Build list based on dob from user settings

--- lib/screens/pathway_event_list_screen.dart
+++ lib/screens/pathway_event_list_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:age_calculator/age_calculator.dart';
+import '../repositories/user_settings_repository.dart';
+import '../models/user_settings.dart';
 import '../models/pathway_event_type.dart';
 import '../models/pathway_event.dart';
 import '../models/pathway_event_date.dart';
@@ -11,7 +13,9 @@ class PathwayEventListScreen extends StatelessWidget {
   final ValueChanged<PathwayEventDate> onPathwayEventDateSelected;
   final ValueChanged<bool> onShowUserDetailSelected;
 
-  const PathwayEventListScreen(
+  final _userSettings = UserSettingsRepository().get();
+
+  PathwayEventListScreen(
       {Key? key,
       required this.pathwayEvents,
       required this.onPathwayEventDateSelected,
@@ -20,7 +24,44 @@ class PathwayEventListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    DateTime dateOfBirth = DateTime(2021, 2, 8);
+    return Scaffold(
+        appBar: AppBar(title: const Text('T21 Combined Care Pathway')),
+        drawer: Drawer(
+            child: ListView(
+          children: [
+            const DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text('T21 Combined Care Pathway',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ))),
+            ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text('Settings'),
+                onTap: () {
+                  Navigator.pop(context);
+                  onShowUserDetailSelected(true);
+                }),
+          ],
+        )),
+        body: FutureBuilder<UserSettings>(
+          future: _userSettings,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const CircularProgressIndicator();
+            }
+
+            List<PathwayMonth> pathwayMonths =
+                _getPathwayMonths(snapshot.data!);
+
+            return _getList(pathwayMonths);
+          },
+        ));
+  }
+
+  List<PathwayMonth> _getPathwayMonths(UserSettings settings) {
+    DateTime dateOfBirth = settings.dateOfBirth ?? DateTime(2021, 2, 8);
     DateTime dateAtAge18 =
         DateTime(dateOfBirth.year + 18, dateOfBirth.month, dateOfBirth.day);
 
@@ -55,63 +96,46 @@ class PathwayEventListScreen extends StatelessWidget {
       }
     } while (currentDate.isBefore(dateAtAge18));
 
-    return Scaffold(
-        appBar: AppBar(title: const Text('T21 Combined Care Pathway')),
-        drawer: Drawer(
-            child: ListView(
-          children: [
-            const DrawerHeader(
-                decoration: BoxDecoration(color: Colors.blue),
-                child: Text('T21 Combined Care Pathway',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ))),
-            ListTile(
-                leading: const Icon(Icons.settings),
-                title: const Text('Settings'),
-                onTap: () {
-                  Navigator.pop(context);
-                  onShowUserDetailSelected(true);
-                }),
-          ],
-        )),
-        body: ListView.builder(
-            padding: const EdgeInsets.all(8.0),
-            itemCount: pathwayMonths.length,
-            itemBuilder: (BuildContext context, int index) {
-              var pathwayMonth = pathwayMonths[index];
-
-              return Column(children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text(pathwayMonth.title,
-                      style: Theme.of(context).textTheme.headline6),
-                ),
-                Column(
-                    children: pathwayMonth.eventDates.map((pathwayEventDate) {
-                  return Card(
-                    child: Column(
-                      children: [
-                        ListTile(
-                            leading: getEventIcon(pathwayEventDate.event.type),
-                            title: Text(pathwayEventDate.event.title),
-                            subtitle: Text(DateFormat.yMMMMd()
-                                .format(pathwayEventDate.date)),
-                            trailing: IconButton(
-                                onPressed: () {
-                                  onPathwayEventDateSelected(pathwayEventDate);
-                                },
-                                icon: const Icon(Icons.info_outline)))
-                      ],
-                    ),
-                  );
-                }).toList())
-              ]);
-            }));
+    return pathwayMonths;
   }
 
-  Icon getEventIcon(PathwayEventType eventType) {
+  ListView _getList(List<PathwayMonth> pathwayMonths) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(8.0),
+        itemCount: pathwayMonths.length,
+        itemBuilder: (BuildContext context, int index) {
+          var pathwayMonth = pathwayMonths[index];
+
+          return Column(children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(pathwayMonth.title,
+                  style: Theme.of(context).textTheme.headline6),
+            ),
+            Column(
+                children: pathwayMonth.eventDates.map((pathwayEventDate) {
+              return Card(
+                child: Column(
+                  children: [
+                    ListTile(
+                        leading: _getEventIcon(pathwayEventDate.event.type),
+                        title: Text(pathwayEventDate.event.title),
+                        subtitle: Text(
+                            DateFormat.yMMMMd().format(pathwayEventDate.date)),
+                        trailing: IconButton(
+                            onPressed: () {
+                              onPathwayEventDateSelected(pathwayEventDate);
+                            },
+                            icon: const Icon(Icons.info_outline)))
+                  ],
+                ),
+              );
+            }).toList())
+          ]);
+        });
+  }
+
+  Icon _getEventIcon(PathwayEventType eventType) {
     if (eventType == PathwayEventType.birthday) {
       return const Icon(Icons.cake);
     }
